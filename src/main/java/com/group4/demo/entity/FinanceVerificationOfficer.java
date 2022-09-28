74729d095d7d8d7a0e96f788b4c9875b38a13b3b
Shreyas Vaghasia <manjuvaghasia0@gmail.com>
2022-09-28 16:41:06 +0530

    Used Lombok  for User,Admin,Customer,LandVerificationOfficer,FinanceVerificationOfficer

--- src/main/java/com/group4/demo/entity/FinanceVerificationOfficer.java
+++ src/main/java/com/group4/demo/entity/FinanceVerificationOfficer.java
@@ -4,18 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class LandVerificationOfficer extends User{
+public class FinanceVerificationOfficer extends  User{
     @Id
     private int userId;
-    private String officeName;
-    private String officeContact;
+    private String finOfficerName;
+    private String finOfficerContact;
 
 
 }
