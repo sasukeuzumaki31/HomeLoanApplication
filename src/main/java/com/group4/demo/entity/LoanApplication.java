060fc4e68ce726e284ae2b5e215572cd0c3ede84
Nimesh <nimeshnairns@gmail.com>
2022-09-29 12:59:25 +0530

    removed code smell from loanApplicationServiceImpl class

--- src/main/java/com/group4/demo/entity/LoanApplication.java
+++ src/main/java/com/group4/demo/entity/LoanApplication.java
@@ -1,5 +1,10 @@
 package com.group4.demo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
@@ -7,6 +12,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class LoanApplication {
 
@@ -20,87 +30,5 @@ public class LoanApplication {
 	private boolean financeVerificationApproval;
 	private boolean adminApproval;
 	private String status;
-
-	public LoanApplication() {
-		super();
-	}
-
-	public LoanApplication(int applicationId, LocalDate applicationDate, double loanAppliedAmount,
-			double loanApprovedAmount, boolean landVerificationApproval, boolean financeVerificationApproval,
-			boolean adminApproval, String status) {
-		super();
-		this.applicationId = applicationId;
-		this.applicationDate = applicationDate;
-		this.loanAppliedAmount = loanAppliedAmount;
-		this.loanApprovedAmount = loanApprovedAmount;
-		this.landVerificationApproval = landVerificationApproval;
-		this.financeVerificationApproval = financeVerificationApproval;
-		this.adminApproval = adminApproval;
-		this.status = status;
-	}
-
-	public long getApplicationId() {
-		return applicationId;
-	}
-
-	public void setApplicationId(int applicationId) {
-		this.applicationId = applicationId;
-	}
-
-	public LocalDate getApplicationDate() {
-		return applicationDate;
-	}
-
-	public void setApplicationDate(LocalDate applicationDate) {
-		this.applicationDate = applicationDate;
-	}
-
-	public double getLoanAppliedAmount() {
-		return loanAppliedAmount;
-	}
-
-	public void setLoanAppliedAmount(double loanAppliedAmount) {
-		this.loanAppliedAmount = loanAppliedAmount;
-	}
-
-	public double getLoanApprovedAmount() {
-		return loanApprovedAmount;
-	}
-
-	public void setLoanApprovedAmount(double loanApprovedAmount) {
-		this.loanApprovedAmount = loanApprovedAmount;
-	}
-
-	public boolean isLandVerificationApproval() {
-		return landVerificationApproval;
-	}
-
-	public void setLandVerificationApproval(boolean landVerificationApproval) {
-		this.landVerificationApproval = landVerificationApproval;
-	}
-
-	public boolean isFinanceVerificationApproval() {
-		return financeVerificationApproval;
-	}
-
-	public void setFinanceVerificationApproval(boolean financeVerificationApproval) {
-		this.financeVerificationApproval = financeVerificationApproval;
-	}
-
-	public boolean isAdminApproval() {
-		return adminApproval;
-	}
-
-	public void setAdminApproval(boolean adminApproval) {
-		this.adminApproval = adminApproval;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
 
 }
