f7fe523efcfeaa569b0c9eb7e3eb39616758355d
Shreyas Vaghasia <manjuvaghasia0@gmail.com>
2022-10-01 11:57:50 +0530

    allowed all 4 signup apis without jwt token

--- src/main/java/com/group4/demo/config/WebSecurityConfig.java
+++ src/main/java/com/group4/demo/config/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // We don't need CSRF for this example
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/authenticate", "/admin/signup").permitAll().
+                .authorizeRequests().antMatchers("/authenticate", "/admin/signup", "/customer/signup", "/landofficer/signup", "/financeofficer/signup").permitAll().
                 // all other requests need to be authenticated
                         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
