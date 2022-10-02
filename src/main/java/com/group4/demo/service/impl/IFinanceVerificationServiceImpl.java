3bb6dcc0d40761c7432eb086f3b8834c01a7649e
Shreyas Vaghasia <manjuvaghasia0@gmail.com>
2022-10-02 12:19:56 +0530

    tried to do role based auth but not yet done

--- src/main/java/com/group4/demo/service/impl/IFinanceVerificationServiceImpl.java
+++ src/main/java/com/group4/demo/service/impl/IFinanceVerificationServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
-
 @Service
 public class IFinanceVerificationServiceImpl implements IFinanceVerificationService {
 
