46a575115e99be0103d9e10a02bc671a1caa75e2
Shreyas Vaghasia <manjuvaghasia0@gmail.com>
2022-10-03 13:09:13 +0530

    Added Logger and testing in ILoanAgreementServiceImpl

--- src/main/java/com/group4/demo/service/impl/ILoanAgreementServiceImpl.java
+++ src/main/java/com/group4/demo/service/impl/ILoanAgreementServiceImpl.java
@@ -4,6 +4,8 @@ import com.group4.demo.advices.ResourceNotFoundException;
 import com.group4.demo.entity.LoanAgreement;
 import com.group4.demo.service.ILoanAgreementService;
 import com.group4.demo.repository.ILoanAgreementRepository;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,34 +14,44 @@ import java.util.List;
 
 @Service
 public class ILoanAgreementServiceImpl implements ILoanAgreementService {
+    Log logger = LogFactory.getLog(ILoanAgreementServiceImpl.class);
 
     @Autowired
     ILoanAgreementRepository loanAggRepo;
 
     @Override
     public LoanAgreement deleteLoanAgreement(long loanAgreementId) throws ResourceNotFoundException {
-        LoanAgreement agreement=loanAggRepo.findById(loanAgreementId).orElseThrow(()->new ResourceNotFoundException("Loan Agreement does not exists with id : "+loanAgreementId));
-            loanAggRepo.delete(agreement);
-            return agreement;
+        logger.info("Entered into deleteLoanAgreement method");
+        LoanAgreement agreement = loanAggRepo.findById(loanAgreementId).orElseThrow(() -> new ResourceNotFoundException("Loan Agreement does not exists with id : " + loanAgreementId));
+        loanAggRepo.delete(agreement);
+        logger.info("Exit from deleteLoanAgreement method");
+
+        return agreement;
     }
 
     @Override
     public List<LoanAgreement> retrieveAllLoanAgreement() {
+        logger.info("Entered into retrieveAllLoanAgreement method");
         return loanAggRepo.findAll();
+
     }
 
     @Override
-    public LoanAgreement retrieveAgreementById(long loanAgreementId) throws ResourceNotFoundException{
-        return loanAggRepo.findById(loanAgreementId).orElseThrow(()->new ResourceNotFoundException("Loan Agreement does not exists with id : "+loanAgreementId));
+    public LoanAgreement retrieveAgreementById(long loanAgreementId) throws ResourceNotFoundException {
+        logger.info("Entered into retrieveAgreementById method" + loanAgreementId);
+
+        return loanAggRepo.findById(loanAgreementId).orElseThrow(() -> new ResourceNotFoundException("Loan Agreement does not exists with id : " + loanAgreementId));
     }
 
     @Override
     public LoanAgreement addLoanAgreement(LoanAgreement loanAgreement) {
+        logger.info("Entered into addLoanAgreement method");
         return loanAggRepo.save(loanAgreement);
     }
 
     @Override
     public LoanAgreement updateLoanAgreement(@Valid LoanAgreement loanAgreement) {
+        logger.info("Entered into updateLoanAgreement method");
         return loanAggRepo.save(loanAgreement);
     }
 }
