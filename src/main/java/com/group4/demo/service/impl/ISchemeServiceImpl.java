843eb956f0399af989896fa2b769a2ec356f0198
Nimesh <nimeshnairns@gmail.com>
2022-10-03 13:19:34 +0530

    Logging added in ISchemeServiceImpl

--- src/main/java/com/group4/demo/service/impl/ISchemeServiceImpl.java
+++ src/main/java/com/group4/demo/service/impl/ISchemeServiceImpl.java
@@ -5,6 +5,8 @@ import com.group4.demo.advices.ResourceNotFoundException;
 import com.group4.demo.entity.Scheme;
 import com.group4.demo.repository.ISchemeRepository;
 import com.group4.demo.service.ISchemeService;
+import org.apache.juli.logging.Log;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,7 @@ import java.util.List;
 @Service
 public class ISchemeServiceImpl implements ISchemeService {
 
+    Log logger = (Log) LoggerFactory.getLogger(ISchemeServiceImpl.class);
     @Autowired
     ISchemeRepository schemeRepo;
 
@@ -26,11 +29,13 @@ public class ISchemeServiceImpl implements ISchemeService {
 
     @Override
     public Scheme getSchemeById(int schemeid) throws ResourceNotFoundException{
+        logger.info("Entered in SchemeById method in ISchemeServiceClass");
         return schemeRepo.findById(schemeid).orElseThrow(() -> new ResourceNotFoundException("Scheme does not exists with id : " + schemeid));
     }
 
     @Override
     public Scheme addScheme(SchemeDto schemeDto) {
+        logger.info("Entered in addScheme method in ISchemeServiceClass");
         Scheme scheme = new Scheme();
         scheme.setInterestRate(schemeDto.getInterestRate());
         scheme.setTenure(schemeDto.getTenure());
@@ -39,6 +44,7 @@ public class ISchemeServiceImpl implements ISchemeService {
 
     @Override
     public Scheme deleteSchemeById(int schemeid) throws ResourceNotFoundException{
+        logger.info("Entered in deleteSchemeById method in ISchemeServiceClass");
         Scheme scheme = schemeRepo.findById(schemeid).orElseThrow(()->new ResourceNotFoundException("Scheme does not exists with id : " + schemeid));
         schemeRepo.delete(scheme);
         return scheme;
@@ -46,6 +52,7 @@ public class ISchemeServiceImpl implements ISchemeService {
 
     @Override
     public Scheme updateScheme(int id, SchemeDto schemeDto) throws ResourceNotFoundException{
+        logger.info("Entered in deleteSchemeById method in ISchemeServiceClass");
         Scheme schemeOp = schemeRepo.findById(id).orElseThrow(()->new ResourceNotFoundException("Scheme does not exists with id : " + id));
             schemeOp.setInterestRate(schemeDto.getInterestRate());
             schemeOp.setTenure(schemeDto.getTenure());
